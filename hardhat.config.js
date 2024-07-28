8fa1bca7e192013fc5f9ae736f22a8cfe63e3c0f
nitin010101-stack <nitindhakad010101@gmail.com>
2024-07-28 16:05:08 -0700

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,18 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+const NEXT_PUBLIC_POLYGON_MUMBAI_RPC = "https://rpc.ankr.com/polygon_amoy";
+const NEXT_PUBLIC_PRIVATE_KEY = "c232137ebe10a9361526a8b8875bc0ec4ee19d517f5da39b546f8b369e814a44";
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.17",
+  networks: {
+    hardhat: {
+      chainId: 80002,
+    },
+    polygon_amoy: {
+      url: NEXT_PUBLIC_POLYGON_MUMBAI_RPC,
+      accounts: [`0x${NEXT_PUBLIC_PRIVATE_KEY}`],
+    },
+  },
+};
